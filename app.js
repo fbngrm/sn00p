309cef913d33f72cce3bdf1a559022098344dee5
fgrimme <fbngrm@gmail.com>
2013-05-28 13:12:58 +0200

    Clean up hard coded auth

--- app.js
+++ app.js
@@ -15,31 +15,6 @@ var DBHandler = require('./db_handler').DBHandler;
 var Fileserver = require('./static_file_server').Fileserver;
 var dateFormat = require('dateformat');
 
-var users = [
-    { id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' }
-  , { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
-];
-
-/*
-function findById(id, fn) {
-  var idx = id - 1;
-  if (users[idx]) {
-    fn(null, users[idx]);
-  } else {
-    fn(new Error('User ' + id + ' does not exist'));
-  }
-}
-
-function findByUsername(username, fn) {
-  for (var i = 0, len = users.length; i < len; i++) {
-    var user = users[i];
-    if (user.username === username) {
-      return fn(null, user);
-    }
-  }
-  return fn(null, null);
-}
-*/
 // Passport session setup.
 //   To support persistent login sessions, Passport needs to be able to
 //   serialize users into and deserialize users out of the session. Typically,
