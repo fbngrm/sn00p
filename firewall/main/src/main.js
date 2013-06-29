889bd5b3d5ac1a01a0ee2166c3b4654b2e07edf2
fgrimme <fbngrm@gmail.com>
2013-06-29 14:55:43 +0200

    REFACTOR: Forward is in router now

--- firewall/main/src/main.js
+++ firewall/main/src/main.js
@@ -14,68 +14,30 @@ var logger = new Logger();
 http.createServer(function(request, response) {
 	sys.log(request.connection.remoteAddress + ": " + request.method + " " + request.url);
 	
-	// check the requests against blacklist
+	// check the requests against black- & whitelist
 	var ip = request.connection.remoteAddress;
+	//ip = '1.1.9';
 	if (permissions.isBanned(ip)) {
 		router.drop(response);
+		msg = "IP " + ip + " is banned";
 		sys.log(msg);
 		return;
 	}
-	// check the requests against whitelist
 	if (!permissions.isAllowed(ip)) {
-		msg = "IP " + ip + " is not allowed to use this proxy";
 		router.deny(response, msg);
+		msg = "IP " + ip + " is not allowed to use this proxy";
 		sys.log(msg);
 		return;
 	}
 	// check the request for brute-force attacks
 	if (bruteForce.check(ip, request.url)) {
 		router.drop(response);
+		msg = "IP " + ip + " is blocked because of too many requests";
 		sys.log(msg);
 		return;
 	}
-	
-	// options for the proxy request
-	request.headers.host = '';
-	var options = {
-		hostname: 'localhost',
-		port: 8080,
-		path: request.url,
-		method: request.method,
-		headers: request.headers
-	};
-  
-	// create the proxy request object
-	var proxy_request = http.request(options); 
-	// add listeners to the proxy request 
-	proxy_request.addListener('response', function (proxy_response) {
-
-		proxy_response.on('data', function(chunk) {
-			response.write(chunk, 'binary');
-		});
-
-		proxy_response.on('end', function() {
-			response.end();
-		});
-
-		proxy_response.on('error', function(error) {
-			sys.log('request.listener - error: ' + error);
-		});
-		
-		response.writeHead(proxy_response.statusCode, proxy_response.headers);
-	});
-	
-	// add the listeners for the requests
-	request.on('data', function(chunk) {
-		proxy_request.write(chunk, 'binary');
-	});
-
-	request.on('end', function() {
-		proxy_request.end();
-	});
- 
-	request.on('error', function(error) {
-	});
+	// if nothing suspicious - forward the request
+	router.forward(request, response);
 	
 }).listen(8081);
 sys.log('Starting proxy firewall on port 8081');
--- firewall/main/src/services/router.js
+++ firewall/main/src/services/router.js
@@ -1,3 +1,4 @@
+var http = require('http');
 
 var Router = function(){
 	
@@ -14,12 +15,52 @@ var Router = function(){
 		response.end();
 	}
 	
-	this.forward = function(response, msg){
+	this.forward = function(request, response) {
+		// options for the proxy request
+		request.headers.host = '';
+		var options = {
+			hostname: 'localhost',
+			port: 8080,
+			path: request.url,
+			method: request.method,
+			headers: request.headers
+		};
+
+		// create the proxy request object
+		var proxy_request = http.request(options); 
+		// add listeners to the proxy request 
+		proxy_request.addListener('response', function (proxy_response) {
 	
+			proxy_response.on('data', function(chunk) {
+				response.write(chunk, 'binary');
+			});
+	
+			proxy_response.on('end', function() {
+				response.end();
+			});
+	
+			proxy_response.on('error', function(error) {
+				sys.log('request.listener - error: ' + error);
+			});
+			
+			response.writeHead(proxy_response.statusCode, proxy_response.headers);
+		});
+		
+		// add the listeners for the requests
+		request.on('data', function(chunk) {
+			proxy_request.write(chunk, 'binary');
+		});
+	
+		request.on('end', function() {
+			proxy_request.end();
+		});
+	 
+		request.on('error', function(error) {
+		});
 	}
 	
 	this.allow = function(response, msg){
-	
+		// ... use for outoing traffic
 	}
 }
 
