ce9e5469ed78fde1fb3ff69168baa348897f8a44
fgrimme <fbngrm@gmail.com>
2013-07-04 23:41:02 +0200

    REFACTOR: add comment to router

--- snoop/main/src/services/router.js
+++ snoop/main/src/services/router.js
@@ -2,11 +2,35 @@ var http = require('http');
 var sys = require('sys');
 var stringUtils = require('../utils/stringutils').StringUtils;
 
+/*
+ * provide options for proxy-requests
+ * address resolution for multiple applications is 
+ * provided by the options argument
+ *
+ * example config:
+ * "router" : {
+ *		"http" : {
+ *			"foo-bar.com" : {
+ *				"hostname" : "localhost",
+ *				"port" : 9000
+ *			}
+ *		}, 
+ *		"https" : {
+ * 			"foo-foo.com" : {
+ *				"hostname" : "localhost",
+ *				"port" : 9021
+ *			}
+ *		}
+ *	}
+ */
+ 
 var Router = function(options){
-	
+	// options for routing
 	var _options = options;
+	// check if options are provided
 	if (!_options) throw 'need route options';
 	
+	// get the proxy-request options by request-host and protocol
 	this.getByHost = function(request, protocol) {
 	
 		// extract hostname from host by removing port(if specified)
@@ -16,11 +40,15 @@ var Router = function(options){
 			var host = request.headers.host;
 		}
 		
+		// get hostname and port from the request
 		try {
 			var hostname = _options[protocol][host]['hostname'];
 			var port = _options[protocol][host]['port'];
-			// options for the proxy request
+			
+			// overwrite the host header in dev-mode or if localhost should be used as host
 			request.headers.host = '';
+			
+			// options for the proxy request
 			return options = {
 				hostname: hostname,
 				port: port,
@@ -29,7 +57,7 @@ var Router = function(options){
 				headers: request.headers
 			};
 		} catch (err) {
-			// BUGFIX: 
+			// BUGFIX: route to 404
 			return {};
 		}
 	};
