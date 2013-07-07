0952ed4c02074d1172668030f8e740b4aae21250
fgrimme <fbngrm@gmail.com>
2013-07-07 13:26:24 +0200

    REFACTOR: comment xss

--- snoop/main/src/snoops/xss.js
+++ snoop/main/src/snoops/xss.js
@@ -1,8 +1,14 @@
 var patterns = require('../conf/patterns').patterns;
 var sys = require('sys');
 
+/*
+ * check if the reuests contains xss signatures 
+ */
 var XSS = function() {
 
+	// check request url, data & cookies(if any) against patterns
+	// return true if detected
+	// else return fase
 	this.check = function(request, response, buffer){
 		
 		for (var i = patterns.xss.length -1; i >= 0; --i) {
