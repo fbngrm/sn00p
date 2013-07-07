3c6f21534b048df81dddcedbe64baf67f494fc9f
fgrimme <fbngrm@gmail.com>
2013-07-07 13:09:46 +0200

    REFACTOR: comment lfi

--- snoop/main/src/snoops/lfi.js
+++ snoop/main/src/snoops/lfi.js
@@ -1,14 +1,18 @@
 var patterns = require('../conf/patterns').patterns;
 var sys = require('sys');
 
+/*
+ * check if the url of the reuests contains patterns 
+ * of directories traversal/local ile inclusion
+ */
+ 
 var LFI = function() {
-
+	// perform the check
+	// return true if patterns are detected else false
 	this.check = function(request, response, buffer){
 		
 		for (var i = patterns.lfi.length -1; i >= 0; --i) {
 			sys.log('lfi check for ip: ' + request.connection.remoteAddress);
-			// check the url
-			sys.log(request.url);
 			if (patterns.lfi[i].test(request.url)) return true;
 		}
 		return false;
