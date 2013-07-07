8fcc066c9ea9a120f2c047b93b56a797bf03bfdb
fgrimme <fbngrm@gmail.com>
2013-07-07 13:29:25 +0200

    REFACTOR: comment stringutils

--- snoop/main/src/utils/stringutils.js
+++ snoop/main/src/utils/stringutils.js
@@ -1,7 +1,10 @@
 
-
+/*
+ * helper functions to work with string objects
+ */
+ 
 exports.StringUtils = new function(){
-
+	// check if the given string contains the given char
 	this.contains = function(str, char) {
 		return str.indexOf(char) !== -1
 	};
